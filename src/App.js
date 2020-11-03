adaa3eb00704ee031146fe6900d901b5f4e05ef2
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-03 13:34:58 +1000

    Added about component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { About } from './components/about'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log("The page is rendering")
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h2 class="portfolio-title">Frank Manue Jr</h2>
+        </header>
+        <main className="main-section">
+          <h3>Main Section</h3>
+          <nav>
+            <ul className="nav-title">
+              <li>about</li>
+              <li>projects</li>
+              <li>contact</li>
+            </ul>
+          </nav>
+          < About />
+          <div>projects</div>
+          <div>contact</div>
+        </main>
+      </div>
+    );
+
+  }
 }
 
 export default App;
---
+++ src/components/about.js
@@ -0,0 +1,19 @@
+// component for About section
+// functional component
+const About = () => {
+    console.log("About component ran");
+    return (
+        <div className="about-section">
+            <h3>About</h3>
+            <p>
+                This is abit about me
+            </p>
+            <p>
+                I love technology, mixed martial arts and multimedia stuff(photography, videography, graphics, etc..)
+            </p>
+
+        </div>
+    );
+}
+
+export {About}
