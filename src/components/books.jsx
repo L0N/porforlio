6531f658ad816c3d2ba274f773990c4fac4d04e0
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-04 15:49:48 +1000

    Create books.jsx

---
+++ src/components/books.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class books extends Component {
+    render(props) {
+        return (
+            <div>
+                <h1>props.booksdata.title</h1>
+            </div>
+        );
+    }
+}
+
+export default books;
