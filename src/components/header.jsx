014b0312b289e51e086754ce3108edaf285ff0b0
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-04 14:05:27 +1000

    Create header.jsx

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class header extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="portfolio-title">Frank Manue Jr</h1>
+                <h2>Web Developer, System Admin & Media Enthusiast</h2>
+            </div>
+        );
+    }
+}
+
+export default header;
