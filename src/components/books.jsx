9db29f53607d25620ba143eecbbdb07b25e4254a
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-06 11:16:45 +1000

    styled books component

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -3,12 +3,10 @@ import React, { Component } from 'react';
 class books extends Component {
     render() {
         console.log("Books component ran")
-        console.log(this.props.title)
         return (
             <div className="book-list">
                 <div className="book-title"> Title: { this.props.title }  </div>
                 <div className="book-author"> Author: { this.props.author }  </div>
-                <div className="book-blurb"> Blurb: { this.props.blurb }  </div>
             </div>
         );
     }
