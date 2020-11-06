ffe46aaa722752b2f97f035f21f24e2e250c1571
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-06 11:06:32 +1000

    add book component with props

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class books extends Component {
-    render(props) {
+    render() {
+        console.log("Books component ran")
+        console.log(this.props.title)
         return (
-            <div>
-                <h1>props.booksdata.title</h1>
+            <div className="book-list">
+                <div className="book-title"> Title: { this.props.title }  </div>
+                <div className="book-author"> Author: { this.props.author }  </div>
+                <div className="book-blurb"> Blurb: { this.props.blurb }  </div>
             </div>
         );
     }
